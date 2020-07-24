8a9648599ad518ad9205e1eeb5278270af6bf6d9
linaksel <linrosaksel@gmail.com>
2020-07-24 20:58:34 +0300

    jsonified

--- server/src/app.py
+++ server/src/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from random import sample
+from xml.etree import ElementTree
+
 app = Flask(__name__)
 
 @app.route('/')
 def funktio():
     return 'Hello, World!'
+
+@app.route('/reader')
+def reader():
+    root = ElementTree.parse('sanalista.xml').getroot()
+    words = []
+    for element in root:
+        word = element[0].text
+        words.append(word)
+    return jsonify(sample(words, 10))
